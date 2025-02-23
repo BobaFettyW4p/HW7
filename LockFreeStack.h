c174844923be9b6cdfddae4b98ef1102ee83013c
Matthew Ivancic <matthew.ivancic91@gmail.com>
2025-02-23 16:29:38 -0600

    updated homework

--- LockFreeStack.h
+++ LockFreeStack.h
@@ -57,10 +57,12 @@ Stack::pop()
 }
 
 /**
- * This is my implementation of the push function for this lock free stack utilizing atomics
+ * This is my implementation of the push function for this lock free stack
  * Following the form of the provided pop function, we begin by loading the current head and initializing a new StackHead object to serve as the new head
  * We then utilize the same design paradigm from the pop function where we attempt to update the head and utilize compare_exchange_weak() to determine whether the head was successfully updated
- * Since this function returns void, we do not need to return anything
+ * One way my push function differs from the pop function is I have utilized modern memory management
+ * As opposed to calling operator new and delete as the pop() function does when the operation is not successful to avoid memory, I am utilizing unique_ptrs to manage the new item
+ * oeprator new and delete are considered thread-safe in C++, but we can lead to undefined behavior were they to fail, so it's better to utilize RAII to manage memory
  */
 void
 Stack::push(int val)
@@ -68,17 +70,19 @@ Stack::push(int val)
     StackHead expected = head.load();
     StackHead newHead;
     bool succeeded = false;
+    std::unique_ptr<StackItem> newItem;
     while(!succeeded) {
-        newHead.link = new StackItem(val);
+        newItem = std::make_unique<StackItem>(val);
+        newHead.link = newItem.get();
         newHead.link->next = expected.link;
         newHead.count = expected.count + 1;
-        succeeded = head.compare_exchange_weak(expected, newHead);
-        // this conditional is tripped if the compare_exchange_weak() evaluates to false
-        // since the operation was not completed on this iteration of the loop, we need to delete the newHead.link to prevent memory leaks
-        if (!succeeded) {
-            delete newHead.link;
-        }
+        succeeded = head.compare_exchange_weak(expected, newHead);   
     }
+    // finally, we release the unique_ptr to the new item
+    // this does carry risks in terms of memory management. Were an exception to happen after release() is called but before the new value is inserted into the stack, a memory leak will occur
+    // since we do not call release() until we have confirmed that the operation is successful, this risk is minimized
+    // a better solution would be to rewrite the entire stack to utilize shared_ptrs, but that's outside the scope of this assignment
+    newItem.release();
 }
 }
 #endif
